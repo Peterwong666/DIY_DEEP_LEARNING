f51b69c9d07bf382e2c50c43e38edf117d679021
Peter Wong <14050256+Peterwong666@users.noreply.github.com>
2019-07-22 13:43:40 +0800

    del all for test

--- Tensorflow/test-mnist.py
+++
@@ -1,7 +0,0 @@
-
-import tensorflow as tf
-
-
-mnist = tf.keras.datasets.mnist #导入mnist
-(x_train,y_train),(x_test,y_test) = mnist.load_data() #分割
-print(x_train[0])
--- Tensorflow/tf_print.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-
-import tensorflow as tf
-try:
-	tf.contrib.eager.enable_eager_execution()
-	print("TF import with eager execution!")
-
-except ValueError:
-	print("TF already imported with eager execution!")
